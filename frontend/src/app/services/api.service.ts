5207f3982d8d5ab8dcf13a90dc270c6cd56a4ee1
RODRIGO <ralarcon@inf.uc3m.es>
2022-11-21 10:16:10 +0100

    update service

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -66,7 +66,7 @@ export class ApiService {
   getDefinitionRae(word: string):Promise<string[]>{
     return new Promise<string[]>((resolve, reject) => {
       try {
-        this.http.get('http://35.240.56.240:5000/api/definition-rae', {
+        this.http.get('http://146.148.118.172:5000/api/definition-rae', {
           params: {
             word: word
           }
